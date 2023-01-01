a442e678c10cfca05be6db8f20e802243f18c4f7
Humaira Hossain <humairahossain@hotmail.com>
2023-01-01 00:39:49 +0000

    finishing up

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# trivia
+# Simple Trivia Program
+
+Project made as Python and API practice.
+
+This CLI quiz app queries https://the-trivia-api.com/ to get all questions, options, and answers. It does not require an access key.  
+
+### Features:
+- Choose how many questions you want to answer
+- Choose a difficulty
+- Quit at any time
+- See score at the end
+- Option to replay
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-def number_of_questions():
-    while True:
-        try:
-            num = int(input('How many questions do you want to answer? '))
+from functions import *
 
-            if num == 0:
-                print('Please enter a number greater than zero')
+
+def main():
+    while True:
+        num = number_of_questions()
+        diff = difficulty()
+
+        url = get_endpoint(num, diff)
+        json = get_json(url)
+
+        question_list = get_questions(json)
+        answer_list = get_correct_answers(json)
+        option_list = get_options(json, answer_list)
+
+        score = 0
+        question_number = 0
+        while question_number < num:
+            question_number += 1
+            display_question(question_list, question_number)
+            display_options(option_list, question_number)
+
+            ans = user_answer()
+
+            if is_correct(ans, option_list, question_number, answer_list):
+                print('CORRECT!')
+                print()
+                score += 1
                 continue
-        except ValueError:
-            print('Please enter a whole number')
-        else:
-            print('Thank you')
+            else:
+                print(f'Incorrect! The right answer was {answer_list[question_number-1]}')
+                print()
+                continue
+
+        print(f'Thanks for playing! You got {score}/{num} right!')
+        if not play_again():
+            print('Goodbye!')
             break
 
-    return num
 
-
-number_of_questions()
+if __name__ == "__main__":
+    main()
